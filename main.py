d57964c1d999905371fbd25c514e8279c8cdde86
Myp3a <murza8965@gmail.com>
2024-05-16 20:13:50 +0300

    feat: unify text input endpoints

--- main.py
+++ main.py
@@ -23,54 +23,43 @@ used_agents_list: list[agents.Agent] = [
     agents.ChatterAgent(llm),
 ]
 
-@routes.post("/generate")
-async def generate_text(req):
-    data = await req.post()
-    if not (history := data.get("history")):
-        return web.json_response({"error": "history is required"}, status=400)
-    history = MessagesWrapper(history.split("\n"))
-    supervisor = agents.SupervisorAgent(llm, used_agents_list)
-    chosen_worker = supervisor.ask(history)
-    match chosen_worker:
-        case "meteorologist":
-            resp = agents.WeatherAgent(llm).ask(history)
-        case "researcher":
-            resp = agents.SearchAgent(llm).ask(history)
-        case "chatter":
-            resp = agents.ChatterAgent(llm).ask(history)
-        case _:
-            resp = "I couldn't understand you. Please, try again."
-    return web.json_response({"text": resp})
-
-async def generate_text_with_history(query, session_id):
-    if (to := config.TRANSLATE):
-        query = await translate(query, to, "en")
-    history = get_history(session_id)
-    history.add_user_message(query)
-    supervisor = agents.SupervisorAgent(llm, used_agents_list)
-    chosen_worker = supervisor.ask(history)
-    match chosen_worker:
-        case "meteorologist":
-            resp = agents.WeatherAgent(llm).ask(history)
-        case "researcher":
-            resp = agents.SearchAgent(llm).ask(history)
-        case "chatter":
-            resp = agents.ChatterAgent(llm).ask(history)
-        case _:
-            resp = "I couldn't understand you. Please, try again."
-    history.add_ai_message(resp)
-    if (to := config.TRANSLATE):
-        resp = await translate(resp, "en", to)
-    return resp
-
 @routes.post("/text_input")
 async def text_input(req: web.Request):
-    data = await req.post()
-    if not (session_id := data.get("session_id")):
-        return web.json_response({"error": "session_id is required"}, status=400)
-    if not (query := data.get("query")):
-        return web.json_response({"error": "query is required"}, status=400)
-    resp = await generate_text_with_history(query, session_id)
+    data = await req.json()
+    session_id = data.get("session_id", None)
+    query = data.get("query", None)
+    history = data.get("history", None)
+    translate_to = data.get("translate", config.TRANSLATE)
+    if not query and not history:
+        return web.json_response({"error": "query or history is required"}, status=400)
+    if not history:
+        if session_id:
+            history = get_history(session_id)
+        else:
+            history = MessagesWrapper([])
+    else:
+        history = MessagesWrapper(history.split("\n"))
+    if query:
+        if translate_to:
+            query = translate(query, translate_to, "en")
+        history.add_user_message(query)
+    
+    supervisor = agents.SupervisorAgent(llm, used_agents_list)
+    chosen_worker = supervisor.ask(history)
+    match chosen_worker:
+        case "meteorologist":
+            resp = agents.WeatherAgent(llm).ask(history)
+        case "researcher":
+            resp = agents.SearchAgent(llm).ask(history)
+        case "chatter":
+            resp = agents.ChatterAgent(llm).ask(history)
+        case _:
+            resp = "I couldn't understand you. Please, try again."
+    
+    if history:
+        history.add_ai_message(resp)
+    if translate_to:
+        resp = await translate(resp, "en", translate_to)
     return web.json_response({"text": resp})
 
 @routes.post("/voice_input")
