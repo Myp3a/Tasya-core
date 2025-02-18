bf598c20ae356fd85152cc34b7f79276378a6235
Myp3a <murza8965@gmail.com>
2025-02-18 23:16:26 +0300

    fix: drop a few loggers

--- tasya/__init__.py
+++ tasya/__init__.py
@@ -1,8 +1,8 @@
 import logging
 
-# Number in %(name){num}s corresponds to the longest .py filename in project folder
+# Number in %(filename){num}s corresponds to the longest .py filename in project folder
 logging.basicConfig(
-    format="%(asctime)s | %(name)14s:%(lineno)3d | %(levelname)8s | %(message)s",
+    format="%(asctime)s | %(filename)14s:%(lineno)3d | %(levelname)8s | %(message)s",
     level=logging.DEBUG,
 )
 
@@ -12,3 +12,7 @@ if supress_library_debug:
     logging.getLogger("urllib3.connectionpool").setLevel(logging.INFO)
     logging.getLogger("httpx").setLevel(logging.INFO)
     logging.getLogger("asyncio").setLevel(logging.INFO)
+    logging.getLogger("httpcore.connection").setLevel(logging.INFO)
+    logging.getLogger("httpcore.http11").setLevel(logging.INFO)
+    logging.getLogger("aiohttp.server").setLevel(logging.INFO)
+    logging.getLogger("aiohttp.access").setLevel(logging.INFO)
